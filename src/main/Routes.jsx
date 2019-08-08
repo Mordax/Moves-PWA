75e6754b9dcc131b450f6aebd785c329b6a8e893
mordax <maya@mordax.io>
2019-08-08 10:20:33 -0400

    Adding routes and page placeholders

---
+++ src/main/Routes.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { BrowserRouter as  Router, Route, Link } from "react-router-dom";
+import App from "../App";
+import About from "./pages/About";
+import EmergencyContact from "./pages/EmergencyContact";
+import IncomingComm from "./pages/IncomingComm";
+import People from "./pages/People";
+import Geolocation from "./pages/Geolocation";
+
+function Routes(){
+  return(
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/About">About</Link></li>
+            <li><Link to="/EmergencyContact">Emergency Contact</Link></li>
+            <li><Link to="/People">People Directory</Link></li>
+            <li><Link to="/IncomingComm">Incoming Communications</Link></li>
+            <li><Link to="/Geolocation">Geolocation</Link></li>
+            {/* Content would be replaced with a dynamic link generator 
+            using JSON file */}
+            <li><Link to="/Content"></Link></li>
+          </ul>
+        </nav>
+        
+        <Route exact path="/" component={App} />
+        <Route path="/About" component={About} />
+        <Route path="/EmergencyContact" component={EmergencyContact} />
+        <Route path="/Geolocation" component={Geolocation} />
+        <Route path="/IncomingComm" component={IncomingComm} />
+        <Route path="/People" component={People} />
+      </div>
+    </Router>
+  );
+}
+
+export default Routes;
---
+++ src/main/pages/Geolocation.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Geolocation(){
+    return(
+        <div>
+            <h2>Geolocation Placeholder</h2>
+        </div>
+    );
+}
+export default Geolocation;
