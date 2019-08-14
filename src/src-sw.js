1fd2e7f3dbb4a4a7a84b7f91c91bb6ecc449c115
rayshroom <goodcolorpencils@gmail.com>
2019-08-14 12:46:41 +0200

    fixed package.json typo

--- src/src-sw.js
+++ src/src-sw.js
@@ -1,5 +1,6 @@
 
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+workbox.precaching.precacheAndRoute([]);
 
 self.addEventListener('message', (event) => {
     if (event.data && event.data.type === 'SKIP_WAITING') {
@@ -21,9 +22,6 @@ self.addEventListener('install', (event) => {
     })
   )
 });
-
-
-workbox.precaching.precacheAndRoute([]);
 
 workbox.routing.registerRoute(
     new RegExp('https://moves-backend-a.herokuapp.com/api/content/slug/*'),
